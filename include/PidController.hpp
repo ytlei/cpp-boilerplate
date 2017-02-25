072e03a9bd52a206404f7b407a526cd869893f12
sshazly <sshazly@ncsu.edu>
2017-02-24 18:13:02 -0600

    FINAL changes

---
+++ include/PidController.hpp
@@ -0,0 +1,37 @@
+/************************************************************
+ *                                                          *
+ * Copyright (C) 2017 by Yi-ting Lei                        *
+ *                                                          *
+ ***********************************************************/
+
+/**
+ *   @file	PidController.hpp
+ *   @brief  	This is a PID controller for controlling car speed.
+ *		one can set target velocity and get current speed.
+ *      
+ *   Control the velocity using kp ki kd parameters in PID algorithm
+ *
+ *   @author	Yi-ting Lei
+ *   @date	2017/2/17
+ *   @author    Sherif Shazly
+ *   @date	2017/2/24
+ */
+#ifndef CLASS_PIDCONTROLLER
+#define CLASS_PIDCONTROLLER
+
+class PidController {
+	private:
+		double kp, ki, kd, dt;
+	public:
+		double initialVelocity, targetVelocity, actualVelocity, integral, prevError;
+		//PidController();
+		void setTargetVelocity(double targetV) ; 	///< set the target velocity
+		void setInitialVelocity(double initialV) ; 	///< set the initial velocity
+		void setActualVelocity(double actualV) ; 	///< set the initial velocity
+		bool setControlParam(double p, double i, double d, double _dt) ; 	///< set the pid parameters
+		double getNextVelocity(); ///< calculate the velocity for the next timestep (currentVelocity + increment)
+		double getActualVelocity();	///< show current (actual) velocity
+		double showCurrentVelocity(double setPoint, double curVelocity,double time) ;	///< show current velocity
+};
+
+#endif
---
+++ readme.md
@@ -0,0 +1,20 @@
+# C++ Boilerplate
+[![Build Status](https://travis-ci.org/ytlei/cpp-boilerplate.svg?branch=master)](https://travis-ci.org/ytlei/cpp-boilerplate)
+[![Coverage Status](https://coveralls.io/repos/github/ytlei/cpp-boilerplate/badge.svg)](https://coveralls.io/github/ytlei/cpp-boilerplate)
+---
+
+## Overview
+
+Simple starter C++ project with:
+
+- cmake
+- googletest
+
+## Installation
+
+- Checkout the repo (and submodules)
+```
+$ git clone --recursive https://github.com/dpiet/cpp-boilerplate.git
+```
+
+@test travis
